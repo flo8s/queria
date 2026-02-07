000767d40b66e32946362015f90681cb120ebb60
UD <u.yudai1202@gmail.com>
2026-02-07 14:31:27 +0900

    refactor: extract read_population_csv macro from raw models

--- transform/models/raw/raw_tsukuba_population_20250401.sql
+++ transform/models/raw/raw_tsukuba_population_20250401.sql
@@ -4,9 +4,6 @@
     )
 }}
 
-select *
-from read_csv_auto(
-    'https://www.city.tsukuba.lg.jp/material/files/group/185/082201_population_20250401_new.csv',
-    header=true,
-    encoding='shift_jis'
-)
+{{ read_population_csv(
+    'https://www.city.tsukuba.lg.jp/material/files/group/185/082201_population_20250401_new.csv'
+) }}
