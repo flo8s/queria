654645a4d574c6f84a628ce448f6eaae58b4612a
UD <u.yudai1202@gmail.com>
2026-02-28 15:24:13 +0900

    feat(zipcode): lg_code にチェックディジットを付与して6桁化
    モジュラス11アルゴリズムで5桁→6桁に変換するマクロを追加し、
    stg_zipcode で適用。k_oxon の境界データ (6桁) と JOIN 可能にする。

--- datasets/zipcode/transform/models/stg/stg_zipcode.sql
+++ datasets/zipcode/transform/models/stg/stg_zipcode.sql
@@ -1,3 +1,19 @@
 {{ config(materialized='view') }}
 
-SELECT * FROM {{ ref('raw_zipcode') }}
+SELECT
+    {{ lg_code_with_check_digit('lg_code') }} AS lg_code,
+    old_zipcode,
+    zipcode,
+    prefecture_kana,
+    city_kana,
+    town_kana,
+    prefecture,
+    city,
+    town,
+    has_multiple_zipcodes,
+    has_koaza_banchi,
+    has_chome,
+    has_multiple_towns,
+    update_status,
+    update_reason
+FROM {{ ref('raw_zipcode') }}
