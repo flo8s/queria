34a9f79c90b8c2ac11552d6f916b9270e7ca3861
UD <u.yudai1202@gmail.com>
2026-03-03 07:50:00 +0900

    refactor(estat): 旧 estat ディレクトリを削除

--- datasets/estat/transform/models/e_stat/mart/estat_items.sql
+++
@@ -1,6 +0,0 @@
-SELECT DISTINCT
-    LEFT(cat01, 1) AS category_prefix,
-    cat01,
-    cat01_metadata->>'$.name' AS item_name
-FROM {{ ref('raw_social_demographic_municipal_basic') }}
-ORDER BY cat01
--- datasets/estat/transform/models/e_stat/raw/raw_social_demographic_municipal_basic.sql
+++
@@ -1,4 +0,0 @@
-SELECT
-    tab, cat01, area, time, unit, value,
-    tab_metadata, cat01_metadata, area_metadata, time_metadata, stat_inf
-FROM {{ source('estat_source', 'social_demographic_municipal_basic') }}
